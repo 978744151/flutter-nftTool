c87cedf003f7282bab0568fa09c13898416096fa
978744151@qq.com <978744151@qq.com>
2025-04-14 20:01:36 +0800

    暂存

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:webview_flutter/webview_flutter.dart';
 // import 'package:webview_flutter_android/webview_flutter_android.dart';
 // import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 // import './utils/http_client.dart';
@@ -12,18 +12,18 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   // 强制设置 WebViewPlatform 实现
-  if (WebViewPlatform.instance == null) {
-    // 如果是 iOS 模拟器或设备，优先使用 WebKitWebView
-    // if (Platform.isIOS || Platform.isMacOS) { // macOS 上运行 iOS 模拟器
-    //   WebViewPlatform.instance = WebKitWebViewPlatform();
-    // }
-    // //  else if (Platform.isAndroid) {
-    //   WebViewPlatform.instance = AndroidWebView();
-    // } else {
-    //   // 默认实现（例如 macOS 或其他平台）
-    //   WebViewPlatform.instance = WebKitWebView(); // 或者根据需要选择
-    // }
-  }
+  // if (WebViewPlatform.instance == null) {
+  // 如果是 iOS 模拟器或设备，优先使用 WebKitWebView
+  // if (Platform.isIOS || Platform.isMacOS) { // macOS 上运行 iOS 模拟器
+  //   WebViewPlatform.instance = WebKitWebViewPlatform();
+  // }
+  // //  else if (Platform.isAndroid) {
+  //   WebViewPlatform.instance = AndroidWebView();
+  // } else {
+  //   // 默认实现（例如 macOS 或其他平台）
+  //   WebViewPlatform.instance = WebKitWebView(); // 或者根据需要选择
+  // }
+  // }
 
   runApp(MyApp());
 }
